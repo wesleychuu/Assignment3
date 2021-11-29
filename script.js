babc0228f836b810d847968224b336b85075e735
wesleychuu <wesleychu@telus.net>
2021-11-28 21:46:08 -0800

    made js code appear less of an eyesore

--- script.js
+++ script.js
@@ -1,334 +1,311 @@
 let contactList = [
-    {
-      name: "Roberta Dobbs",
-      phone: "778-555-1234",
-      address: "101 Main St, Anytown, USA",
-      email: "subgenius@slack.example.com",
-    }, 
-    {
-      name: "Bugs Bunny",
-      phone: "123-867-5309",
-      address: "Warner Brothers Animation Lot",
-      email: "whatsup@doc.example.com",
-    },
+  {
+    name: "Roberta Dobbs",
+    phone: "778-555-1234",
+    address: "101 Main St, Anytown, USA",
+    email: "subgenius@slack.example.com",
+  },
+  {
+    name: "Bugs Bunny",
+    phone: "123-867-5309",
+    address: "Warner Brothers Animation Lot",
+    email: "whatsup@doc.example.com",
+  },
 ];
 
 function cleanUpIndex() {
-    elements = document.querySelectorAll('.contact');
+  elements = document.querySelectorAll(".contact");
 
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].remove();
-    }
-};
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].remove();
+  }
+}
 
 function createSingleIndex(contact) {
+  let main = document.querySelectorAll(".main");
 
-    let main = document.querySelectorAll('.main');
+  let createA = document.createElement("a");
+  let createDiv = document.createElement("div");
+  let createP = document.createElement("p");
 
-    let createA = document.createElement('a');
-    let createDiv = document.createElement('div');
-    let createP = document.createElement('p')
+  createP.append(contact.name);
+  createDiv.appendChild(createP);
+  createDiv.classList.add("contact");
 
-    createP.append(contact.name);
-    createDiv.appendChild(createP);
-    createDiv.classList.add("contact");
+  createA.appendChild(createDiv);
+  createA.href = "page3.html";
 
-    createA.appendChild(createDiv);
-    createA.href = "page3.html";
+  main[0].appendChild(createA);
 
-    main[0].appendChild(createA);
-
-    createA.addEventListener("click", function(event) {
-        event.preventDefault();
-        cleanUpIndex();
-        renderView(contact);
-    })
-};
+  createA.addEventListener("click", function (event) {
+    event.preventDefault();
+    cleanUpIndex();
+    renderView(contact);
+  });
+}
 
 function renderIndex(contactList) {
+  let main = document.querySelectorAll(".main")[0];
 
-    let main = document.querySelectorAll('.main')[0];
-       
-    for (let i = 0; i < contactList.length; i++) {
-        createSingleIndex(contactList[i]);
-    }
-
-
-};
-
-
+  for (let i = 0; i < contactList.length; i++) {
+    createSingleIndex(contactList[i]);
+  }
+}
 
 function cleanUpView() {
-    let contactinfo = document.querySelectorAll('.contactinfo');
-    contactinfo[0].remove();    
+  let contactinfo = document.querySelectorAll(".contactinfo");
+  contactinfo[0].remove();
 }
 
 function renderView(contact) {
-    let main = document.querySelectorAll('.main')[0];
-    
-    let divInfo = document.createElement('div');
-    let divName = document.createElement('div');
-    let createImg = document.createElement('img');
-    let divEmail = document.createElement('div');
-    let divPhone = document.createElement('div');
-    let divAddress = document.createElement('div');
-    let divButtons = document.createElement('div');
-    let buttonEdit = document.createElement('button')
-    let buttonClose = document.createElement('button')
+  let main = document.querySelectorAll(".main")[0];
 
-    createImg.src = './img/profile.jpg';
-    createImg.classList.add('profilepic');
-    createImg.alt = 'Profile Picture';
+  let divInfo = document.createElement("div");
+  let divName = document.createElement("div");
+  let createImg = document.createElement("img");
+  let divEmail = document.createElement("div");
+  let divPhone = document.createElement("div");
+  let divAddress = document.createElement("div");
+  let divButtons = document.createElement("div");
+  let buttonEdit = document.createElement("button");
+  let buttonClose = document.createElement("button");
 
-    divName.append(contact.name);
-    divName.classList.add('contactname');
+  createImg.src = "./img/profile.jpg";
+  createImg.classList.add("profilepic");
+  createImg.alt = "Profile Picture";
 
-    divInfo.classList.add('contactinfo');
+  divName.append(contact.name);
+  divName.classList.add("contactname");
 
-    divName.appendChild(createImg);
-    divInfo.appendChild(divName);
-    main.appendChild(divInfo);
+  divInfo.classList.add("contactinfo");
 
-    divEmail.append('email: ', contact.email);
-    divEmail.classList.add('contactemail');
+  divName.appendChild(createImg);
+  divInfo.appendChild(divName);
+  main.appendChild(divInfo);
 
-    divInfo.appendChild(divEmail);
+  divEmail.append("email: ", contact.email);
+  divEmail.classList.add("contactemail");
 
-    divPhone.append('cell: ', contact.phone);
-    divPhone.classList.add('contactphone');
+  divInfo.appendChild(divEmail);
 
-    divInfo.appendChild(divPhone);
+  divPhone.append("cell: ", contact.phone);
+  divPhone.classList.add("contactphone");
 
-    divAddress.append('address: ', contact.address);
-    divAddress.classList.add('contactaddress');
+  divInfo.appendChild(divPhone);
 
-    divInfo.appendChild(divAddress);
+  divAddress.append("address: ", contact.address);
+  divAddress.classList.add("contactaddress");
 
-    buttonEdit.classList.add('button');
-    buttonEdit.classList.add('edit');
-    buttonEdit.value = 'Edit';
-    buttonEdit.append('Edit');
+  divInfo.appendChild(divAddress);
 
-    buttonClose.classList.add('button');
-    buttonClose.classList.add('close');
-    buttonClose.value = 'Close';
-    buttonClose.append('Close');
+  buttonEdit.classList.add("button");
+  buttonEdit.classList.add("edit");
+  buttonEdit.value = "Edit";
+  buttonEdit.append("Edit");
 
-    divButtons.classList.add('buttons');
-    divButtons.appendChild(buttonEdit);
-    divButtons.appendChild(buttonClose);
+  buttonClose.classList.add("button");
+  buttonClose.classList.add("close");
+  buttonClose.value = "Close";
+  buttonClose.append("Close");
 
-    divInfo.appendChild(divButtons);
+  divButtons.classList.add("buttons");
+  divButtons.appendChild(buttonEdit);
+  divButtons.appendChild(buttonClose);
 
+  divInfo.appendChild(divButtons);
 
-
-    buttonClose.addEventListener('click', function(event) {
-        event.preventDefault();
-        cleanUpView();
-        renderIndex(contactList);
-    })    
+  buttonClose.addEventListener("click", function (event) {
+    event.preventDefault();
+    cleanUpView();
+    renderIndex(contactList);
+  });
 }
 
-
-
-
 function cleanUpCreate() {
-    let contactedit = document.querySelectorAll(".contactedit")
+  let contactedit = document.querySelectorAll(".contactedit");
 
-    contactedit[0].remove()
+  contactedit[0].remove();
 }
 
 function renderCreate(contact) {
-    let main = document.querySelectorAll(".main")
+  let main = document.querySelectorAll(".main");
 
-    let contactEdit = document.createElement("div");
-    contactEdit.classList.add("contactedit")
+  let contactEdit = document.createElement("div");
+  contactEdit.classList.add("contactedit");
 
-    let contactImg = document.createElement("div");
-    contactImg.classList.add("contactimg")
+  let contactImg = document.createElement("div");
+  contactImg.classList.add("contactimg");
 
-    let profilePic = document.createElement("img");
-    profilePic.classList.add("profilepic");
-    profilePic.src = "./img/profile.jpg";
-    profilePic.alt = "Profile picture";
-    contactImg.appendChild(profilePic);
+  let profilePic = document.createElement("img");
+  profilePic.classList.add("profilepic");
+  profilePic.src = "./img/profile.jpg";
+  profilePic.alt = "Profile picture";
+  contactImg.appendChild(profilePic);
 
-    let form = document.createElement("form");
+  let form = document.createElement("form");
 
-    let inputContainer1 = document.createElement("div");
-    inputContainer1.classList.add("inputcontainer");
-    form.appendChild(inputContainer1)
+  let inputContainer1 = document.createElement("div");
+  inputContainer1.classList.add("inputcontainer");
+  form.appendChild(inputContainer1);
 
-    let contactName = document.createElement("input")
-    contactName.type = "text";
-    contactName.id = "contactname";
-    contactName.name = "contactname";
-    contactName.placeholder = "Contact Name";
-    // contactName.value = contact.name;
-    inputContainer1.appendChild(contactName);
+  let contactName = document.createElement("input");
+  contactName.type = "text";
+  contactName.id = "contactname";
+  contactName.name = "contactname";
+  contactName.placeholder = "Contact Name";
+  // contactName.value = contact.name;
+  inputContainer1.appendChild(contactName);
 
-    let extraNameField = document.createElement("button")
-    extraNameField.classList.add("extrafield");
-    extraNameField.id = "extranamefield";
-    extraNameField.name = "extranamefield";
-    extraNameField.append("+");
-    inputContainer1.appendChild(extraNameField);
+  let extraNameField = document.createElement("button");
+  extraNameField.classList.add("extrafield");
+  extraNameField.id = "extranamefield";
+  extraNameField.name = "extranamefield";
+  extraNameField.append("+");
+  inputContainer1.appendChild(extraNameField);
 
-    let inputContainer2 = document.createElement("div");
-    inputContainer2.classList.add("inputcontainer");
-    form.appendChild(inputContainer2);
+  let inputContainer2 = document.createElement("div");
+  inputContainer2.classList.add("inputcontainer");
+  form.appendChild(inputContainer2);
 
-    let contactPhone = document.createElement("input");
-    contactPhone.type = "tel";
-    contactPhone.id = "contactphone";
-    contactPhone.name = "contactphone";
-    contactPhone.placeholder ="Contact Phone";
-    // contactPhone.value = contact.phone;
-    inputContainer2.appendChild(contactPhone);
+  let contactPhone = document.createElement("input");
+  contactPhone.type = "tel";
+  contactPhone.id = "contactphone";
+  contactPhone.name = "contactphone";
+  contactPhone.placeholder = "Contact Phone";
+  // contactPhone.value = contact.phone;
+  inputContainer2.appendChild(contactPhone);
 
-    let extraPhoneField = document.createElement("button")
-    extraPhoneField.classList.add("extrafield");
-    extraPhoneField.id = "extraphonefield";
-    extraPhoneField.name = "extraphonefield";
-    extraPhoneField.append("+");
-    inputContainer2.appendChild(extraPhoneField);
+  let extraPhoneField = document.createElement("button");
+  extraPhoneField.classList.add("extrafield");
+  extraPhoneField.id = "extraphonefield";
+  extraPhoneField.name = "extraphonefield";
+  extraPhoneField.append("+");
+  inputContainer2.appendChild(extraPhoneField);
 
+  let inputContainer3 = document.createElement("div");
+  inputContainer3.classList.add("inputcontainer");
+  form.appendChild(inputContainer3);
 
-    let inputContainer3 = document.createElement("div");
-    inputContainer3.classList.add("inputcontainer");
-    form.appendChild(inputContainer3);
+  let contactAddress = document.createElement("input");
+  contactAddress.type = "text";
+  contactAddress.id = "contactaddress";
+  contactAddress.name = "contactaddress";
+  contactAddress.placeholder = "Contact Address";
+  // contactAddress.value = contact.address;
+  inputContainer3.appendChild(contactAddress);
 
-    let contactAddress = document.createElement("input");
-    contactAddress.type = "text";
-    contactAddress.id = "contactaddress";
-    contactAddress.name = "contactaddress";
-    contactAddress.placeholder = "Contact Address";
-    // contactAddress.value = contact.address;
-    inputContainer3.appendChild(contactAddress);
+  let extraAddressField = document.createElement("button");
+  extraAddressField.classList.add("extrafield");
+  extraAddressField.id = "extraaddressfield";
+  extraAddressField.name = "extraaddressfield";
+  extraAddressField.append("+");
+  inputContainer3.appendChild(extraAddressField);
 
-    let extraAddressField = document.createElement("button");
-    extraAddressField.classList.add("extrafield");
-    extraAddressField.id = "extraaddressfield";
-    extraAddressField.name = "extraaddressfield";
-    extraAddressField.append("+");
-    inputContainer3.appendChild(extraAddressField);
+  let inputContainer4 = document.createElement("div");
+  inputContainer4.classList.add("inputcontainer");
+  form.appendChild(inputContainer4);
 
-    let inputContainer4 = document.createElement("div");
-    inputContainer4.classList.add("inputcontainer");
-    form.appendChild(inputContainer4);
+  let contactEmail = document.createElement("input");
+  contactEmail.type = "email";
+  contactEmail.id = "contactemail";
+  contactEmail.name = "contactemail";
+  contactEmail.placeholder = "Contact Email";
+  // contactEmail.value = contact.email;
+  inputContainer4.appendChild(contactEmail);
 
-    let contactEmail = document.createElement("input")
-    contactEmail.type = "email";
-    contactEmail.id = "contactemail";
-    contactEmail.name = "contactemail";
-    contactEmail.placeholder = "Contact Email";
-    // contactEmail.value = contact.email;
-    inputContainer4.appendChild(contactEmail);
+  let extrafield4 = document.createElement("button");
+  extrafield4.classList.add("extrafield");
+  extrafield4.id = "extraemailfield";
+  extrafield4.name = "extraemailfield";
+  extrafield4.append("+");
+  inputContainer4.appendChild(extrafield4);
 
-    let extrafield4 = document.createElement("button");
-    extrafield4.classList.add("extrafield");
-    extrafield4.id = "extraemailfield";
-    extrafield4.name = "extraemailfield";
-    extrafield4.append("+");
-    inputContainer4.appendChild(extrafield4);
+  let buttons = document.createElement("div");
+  buttons.classList.add("buttons");
+  form.appendChild(buttons);
 
-    let buttons = document.createElement("div");
-    buttons.classList.add("buttons");
-    form.appendChild(buttons);
+  let buttonSave = document.createElement("button");
+  buttonSave.classList.add("button");
+  buttonSave.classList.add("save");
+  buttonSave.type = "submit";
+  buttonSave.id = "savecontact";
+  buttonSave.name = "savecontact";
+  buttonSave.append("Save Contact");
+  buttons.appendChild(buttonSave);
 
-    let buttonSave = document.createElement("button");
-    buttonSave.classList.add("button");
-    buttonSave.classList.add("save");
-    buttonSave.type = "submit";
-    buttonSave.id = "savecontact";
-    buttonSave.name = "savecontact";
-    buttonSave.append("Save Contact");
-    buttons.appendChild(buttonSave);
+  let buttonCancel = document.createElement("button");
+  buttonCancel.type = "button";
+  buttonCancel.classList.add("button");
+  buttonCancel.classList.add("save");
+  buttonCancel.id = "cancel";
+  buttonCancel.name = "cancel";
+  buttonCancel.value = "Close";
+  buttonCancel.append("Cancel");
+  buttons.appendChild(buttonCancel);
 
-    let buttonCancel = document.createElement("button");
-    buttonCancel.type = "button";
-    buttonCancel.classList.add("button");
-    buttonCancel.classList.add("save");
-    buttonCancel.id = "cancel";
-    buttonCancel.name = "cancel"
-    buttonCancel.value = "Close";
-    buttonCancel.append("Cancel");
-    buttons.appendChild(buttonCancel);
+  contactEdit.appendChild(contactImg);
+  contactEdit.appendChild(form);
 
-    contactEdit.appendChild(contactImg);
-    contactEdit.appendChild(form);
+  main[0].appendChild(contactEdit);
 
-    main[0].appendChild(contactEdit);
+  buttonCancel.addEventListener("click", function (event) {
+    event.preventDefault();
+    cleanUpCreate();
+    renderIndex(contactList);
+  });
 
+  buttonSave.addEventListener("click", function (event) {
+    event.preventDefault();
+    let myForm = document.querySelector("form");
+    let formName = myForm.elements[0].value;
+    let formPhone = myForm.elements[2].value;
+    let formAddress = myForm.elements[4].value;
+    let formEmail = myForm.elements[6].value;
+    let myinfo = {
+      name: formName,
+      phone: formPhone,
+      address: formAddress,
+      email: formEmail,
+    };
 
-
-    buttonCancel.addEventListener('click', function(event) {
-        event.preventDefault();
-        cleanUpCreate();
-        renderIndex(contactList);
-    })
-
-    buttonSave.addEventListener('click', function(event) {
-        event.preventDefault();
-        let myForm = document.querySelector('form');
-        let formName = myForm.elements[0].value;
-        let formPhone = myForm.elements[2].value;
-        let formAddress = myForm.elements[4].value;
-        let formEmail = myForm.elements[6].value;
-        let myinfo = {
-            name: formName,
-            phone: formPhone,
-            address: formAddress,
-            email: formEmail,
-        };
-
-        contactList.push(myinfo);
-        cleanUpCreate();
-        renderView(contactList[contactList.length-1]);
-    })
+    contactList.push(myinfo);
+    cleanUpCreate();
+    renderView(contactList[contactList.length - 1]);
+  });
 }
 
+let contactsHome = document.querySelector("#contactshome");
 
+contactsHome.addEventListener("click", function (event) {
+  event.preventDefault();
+  let create = document.querySelectorAll(".contactedit");
+  let view = document.querySelectorAll(".contactinfo");
+  let index = document.querySelectorAll(".contact");
+  if (create.length != 0) {
+    cleanUpCreate();
+  } else if (view.length != 0) {
+    cleanUpView();
+  } else if (index.length != 0) {
+    cleanUpIndex();
+  }
+  renderIndex(contactList);
+});
 
+let newContact = document.querySelector("#newcontact");
 
-
-let contactsHome = document.querySelector('#contactshome');
-
-contactsHome.addEventListener('click', function(event) {    
-    event.preventDefault();
-    let create = document.querySelectorAll(".contactedit");
-    let view = document.querySelectorAll(".contactinfo");
-    let index = document.querySelectorAll(".contact");
-    if (create.length != 0) {
-        cleanUpCreate();
-    } 
-    else if (view.length != 0) {
-        cleanUpView();
-    } 
-    else if (index.length != 0) {
-        cleanUpIndex();
-    }
-    
-})
-
-let newContact = document.querySelector('#newcontact');
-
-newContact.addEventListener('click', function(event) {
-    event.preventDefault();
-    let create = document.querySelectorAll(".contactedit");
-    let view = document.querySelectorAll(".contactinfo");
-    let index = document.querySelectorAll(".contact");
-    if (create.length != 0) {
-        cleanUpCreate();
-    } 
-    else if (view.length != 0) {
-        cleanUpView();
-    } 
-    else if (index.length != 0) {
-        cleanUpIndex();
-    }
-    renderCreate();   
-})
-
+newContact.addEventListener("click", function (event) {
+  event.preventDefault();
+  let create = document.querySelectorAll(".contactedit");
+  let view = document.querySelectorAll(".contactinfo");
+  let index = document.querySelectorAll(".contact");
+  if (create.length != 0) {
+    cleanUpCreate();
+  } else if (view.length != 0) {
+    cleanUpView();
+  } else if (index.length != 0) {
+    cleanUpIndex();
+  }
+  renderCreate();
+});
 
 renderIndex(contactList);
